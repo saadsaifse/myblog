cb8504dceeddcedd522f7f5cf53e97f10698b3ad
Saad Saif <msaadsaif5@gmail.com>
2020-01-30 21:10:53 +0100

    update package config, code clean

--- src/templates/page.js
+++ src/templates/page.js
@@ -6,9 +6,7 @@ import SEO from "../components/SEO"
 import config from "../../site-config"
 
 const PageTemplate = props => {
-  console.log(props)
   const { slug } = props.pageContext
-  console.log(slug)
   const postNode = props.data.markdownRemark
   const page = postNode.frontmatter
 
