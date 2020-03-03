eeebd68b0154a02ad1758d51471a413de406e83d
Saad Saif <msaadsaif5@gmail.com>
2020-03-03 22:43:38 +0100

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Saad's Blog`,
+    description: `Personal blog of Saad Saif`,
+    author: `@saadsaifse`,
   },
+  pathPrefix: "/myblog",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
